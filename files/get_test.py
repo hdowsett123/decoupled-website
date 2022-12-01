914a79586ec2491c007c178aeab2c32ead27fc27
Harry Dowsett <88186800+hdowsett123@users.noreply.github.com>
2022-12-01 19:36:17 +0200

    Update with region

--- files/get_test.py
+++ files/get_test.py
@@ -2,6 +2,8 @@ import boto3
 import json
 from boto3.dynamodb.conditions import Key
 
+region = us-east-1
+
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('cloud-resume-challenge')
 
